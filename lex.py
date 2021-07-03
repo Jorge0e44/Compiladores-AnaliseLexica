505f052ef2ba6e05552cc8975a5e7a9d457e804c
Jorge0e44 <33666874+Jorge0e44@users.noreply.github.com>
2021-07-03 17:07:30 -0300

    Update lex.py
    ADICIONEI MAIS TOKENS

--- lex.py
+++ lex.py
@@ -1,3 +1,4 @@
+from os import altsep
 import re
 
 s = input("Enter filename : ")
@@ -11,14 +12,36 @@ PalavrasReservadas = ['auto','break', 'case', 'char', 'const', 'continue', 'defa
 			'double', 'else', 'enum', 'extern', 'float', 'for', 'goto', 'if', 
 			'int', 'long', 'register', 'return', 'short', 'signed', 'sizeof', 'static', 
 			'struct', 'switch', 'typedef', 'union', 'unsigned', 'void', 'volatile', 'while']
+simbolos = ['@', '#', '$', '&', '^']
+operadores_logicos = ['&&','||','!']##AND ORD NOT
+operadores_aritmeticos=['+','-','*','/','%']#VERIFICAR A DIVISÃO PQ TÁ BUGANDO
+operadores_relacionais = [ '+=','!=','-=', '==', '<', '>', '<=', '>=','<>']#VERIFICAR QUANDO O USO FOR DO TIPO <STDIO.H> POIS O <> ELE IDENTIFICA SOMENTE COMO OPERADOR RELACIONAL
+tipo_variaveis=['int','float','long','double','short','char','long','unsigned']
+simb_atribuicao = ['=']
+loops = ['for','while'] #verificar se o do entra nesse caso
+condicionais = ['if','else']
+digitos = ['0','1','2','3','4','5','6','7','8','9']
+PalavrasReservadas = ['auto','break', 'case', 'const', 'continue', 'default', 'do', 
+			'enum', 'extern', 'goto','define',
+			  'register', 'return', 'signed', 'sizeof', 'static','include','main'
+			'struct', 'switch', 'typedef', 'union', 'void', 'volatile']
 delimitadores = [' ', '	', '.', ',', '\n', ';', '(', ')', '<', '>', '{', '}', '[', ']']
 
 
 in_PalavrasReservadas = []
 in_spl_Simbolos= []
-in_oparadores = []
+in_operadores_logicos=[]
+in_operadores_aritmeticos= []
+in_operadores_relacionais = []
+in_tipo_variaveis = []
+in_simb_atribuicao = []
+in_tipo_constantes = []
+in_loops = []
+in_condicionais = []
+in_digitos = []
 in_delimitadores = []
 in_identificadores = []
+in_entremaiormenor = []
 in_constantes = []
 
 tokens = []
@@ -28,7 +51,7 @@ isCmt = 0
 token = ''
 
 for i in text:
-	if i == '/':
+	if i == '/': #FAZ O TESTE PARA VERIFICAR SE É UM COMENTÁRIO
 		isCmt = isCmt+1
 
 	elif isCmt == 2:
@@ -52,7 +75,7 @@ for i in text:
 		isWord = True
 		token = i
     
-	elif (i in delimitadores) or (i in oparadores):
+	elif (i in delimitadores) or (i in operadores_aritmeticos) or (i in operadores_logicos) or (i in operadores_relacionais) or (i in loops) or (i in condicionais):
 		if token:
 			tokens.append(token)
 			token = ''
@@ -68,13 +91,34 @@ for token in tokens:
 	if token in simbolos:
 		in_spl_Simbolos.append(token)
 
-	elif token in oparadores:
-		in_oparadores.append(token)
+	elif token in operadores_relacionais:
+		in_operadores_relacionais.append(token)
+	
+	elif token in operadores_aritmeticos:
+		in_operadores_aritmeticos.append(token)
+	
+	elif token in operadores_logicos:
+		in_operadores_logicos.append(token)
 
+	elif token in tipo_variaveis:
+		in_tipo_variaveis.append(token)
+	
+	elif token in simb_atribuicao:
+		in_simb_atribuicao.append(token)
+	
+	elif token in loops:
+		in_loops.append(token)
+	elif token in condicionais:
+		in_condicionais.append(token)
+	
+	elif token in digitos:
+		in_digitos.append(token)
+	
 	elif token in PalavrasReservadas:
 		in_PalavrasReservadas.append(token)
-				
-	elif re.search("^[_a-zA-Z][_a-zA-Z0-9]*$",token):
+
+	#O USO DO [] CORRESPONDE QUE QUEREMOS PEGAR UM CONJUNTO DE CARACTERES			
+	elif re.search("^[_a-zA-Z][_a-zA-Z]*$",token):  
 		in_identificadores.append(token)
 		
 	elif token in delimitadores:
@@ -84,17 +128,48 @@ for token in tokens:
 		in_constantes.append(token)
 	
 						
-print("NUMERO DE TOKENS  = ", len(tokens))   
+print("NUMERO DE TOKENS  = ", len(tokens))  
+
 print("\nNUMERO DE PALAVRAS RESERVADAS  = ",len(in_PalavrasReservadas))
 print(in_PalavrasReservadas);
+
 print("\nNNUMERO DE SIMBOLOS ESPECIAIS = ",len(in_spl_Simbolos))
 print(in_spl_Simbolos);
-print("\nNUMERO DE OPERADORES = ",len(in_oparadores))
-print(in_oparadores);
-print("\nNUMERO DE IDENTIFICADORES  = ",len(in_identificadores))
+
+print("\nNUMERO DE OPERADORES LÓGICOS  = ",len(in_operadores_logicos))
+print(in_operadores_logicos);
+
+print("\nNUMERO DE OPERADORES ARITMÉTICOS  = ",len(in_operadores_aritmeticos))
+print(in_operadores_aritmeticos);
+
+print("\nNUMERO DE OPERADORES RELACIONAIS  = ",len(in_operadores_relacionais))
+print(in_operadores_relacionais);
+
+print("\nNUMERO DE TIPO DE VARIAVEIS  = ",len(in_tipo_variaveis))
+print(in_tipo_variaveis);
+
+print("\nNUMERO DE SIMBULOS DE ATRIBUICAO = ",len(in_simb_atribuicao))
+print(in_simb_atribuicao);
+
+print("\nNUMERO DE LOOPS = ",len(in_loops))
+print(in_loops);
+
+print("\nNUMERO DE SIMBULOS CONDICIONAIS= ",len(in_condicionais))
+print(in_condicionais);
+
+print("\nNUMERO DE DIGITOS  = ",len(in_digitos))
+print(in_digitos);
+
+print("\nNUMERO DE IDENTIFICADROES = ",len(in_identificadores))
 print(in_identificadores);
+
 print("\nNUMERO DE CONSTANTES = ",len(in_constantes))
 print(in_constantes);
+
+#VERIFICAR COM O GIU SOBRE ESSE CONSTANTES, PQ ME PARECE Q ELE TÁ IMPRIMINDO OUTRAS COISAS
+#print("\nNUMERO DE CONSTANTES = ",len(in_constantes))
+#print(in_constantes);
+
 print("\nNUMERO DE DELIMITADORES = ",len(in_delimitadores))
 print(in_delimitadores);
 f.close()   
